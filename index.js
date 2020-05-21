0fed06f790eb2a4d9db3d3592503d12445dc9046
Raader <48628713+Raader@users.noreply.github.com>
2020-05-21 15:47:33 +0300

    complete layout

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-let gTable = document.getElementsByClassName("game-table")[0];
-for (let i = 0; i < 16; i++) {
-    gTable.insertRow();
-    for (let t = 0; t < 16; t++) {
-        let button = document.createElement("button");
-        button.innerHTML = "";
-        button.className = "game-button";
-        let cell = gTable.rows[i].insertCell();
-        cell.appendChild(button);
+function generateButtons(height, widht, mineCount, table) {
+    table.innerHTML = "";
+    console.log("generating");
+    for (let i = 0; i < height; i++) {
+        gTable.insertRow();
+        for (let t = 0; t < widht; t++) {
+            let button = document.createElement("button");
+            button.innerHTML = "";
+            button.className = "game-button";
+            let cell = gTable.rows[i].insertCell();
+            cell.appendChild(button);
+        }
     }
 }
+
+function InitGame() {
+    generateButtons(heightInput.value, widhtInput.value, 0, gTable);
+}
+let gTable = document.getElementsByClassName("game-table")[0];
+const heightInput = document.querySelector("#heightInput");
+const widhtInput = document.querySelector("#widhtInput");
+const generateButton = document.getElementById("generateButton");
+
+
+generateButton.addEventListener("click", InitGame);
+InitGame();
